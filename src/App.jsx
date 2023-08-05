565883b13ce7374e8b0e7b8ac77c7f6115b74954
GraemeAngusJones <48164351+graemeajones@users.noreply.github.com>
2023-08-05 14:46:03 +0100

    Written todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { useState, useRef, useEffect } from 'react';
 import './App.scss';
 
 function App() {
-  return <p>Hello World!</p>;
+  // State
+  const [todos, setTodos] = useState([]);
+
+  // Lifecycle
+  useEffect(() => {
+    const storedTodos = localStorage.getItem('todos');
+    setTodos(storedTodos ? JSON.parse(storedTodos) : []);
+  }, []);
+
+  // Bindings
+  const newTodo = useRef();
+
+  // Events
+  const handleAdd = () => {
+    const extendedTodos = [...todos, newTodo.current.value];
+    setTodos(extendedTodos); // set is async i.e. old state will persist for a while
+    localStorage.setItem('todos', JSON.stringify(extendedTodos));
+  };
+
+  return (
+    <div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
+      <div>
+        <input type="text" ref={newTodo}></input>
+        <button onClick={handleAdd}>Add</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
